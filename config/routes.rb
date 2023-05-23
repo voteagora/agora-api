8198cab078cba4a52c0105f577fecfc5a7eec06b
Kent Fenwick <kent.fenwick@gmail.com>
2023-05-23 17:07:07 -0400

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,18 @@ Rails.application.routes.draw do
       # Users
       get "users", to: "users#index"
       get "users/:address", to: "users#show"
+      get "users/search/:term", to: "users#search"
       # Proposals
       get "proposals", to: "proposals#index"
       get "proposals/:uuid", to: "proposals#show"
+      get "proposals/search/:term", to: "proposals#search"
+      # Votes
+      get "proposals/:uuid/votes", to: "votes#by_proposal"
+      get "users/:address/votes", to: "votes#by_user"
+      # Delegations
+      get "users/:address/delegations", to: "delegations#by_user"
+      get "users/:address/delegations_received", to: "delegations#by_user_received"
+      
     end
   end
   
