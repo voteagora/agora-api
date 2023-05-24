7621c1ee660a9aa9eea4cd889c267ee88092579e
Kent Fenwick <kent.fenwick@gmail.com>
2023-05-24 09:31:28 -0400

    added vote action for proposals

--- app/controllers/api/v1/proposals_controller.rb
+++ app/controllers/api/v1/proposals_controller.rb
@@ -18,4 +18,16 @@ class Api::V1::ProposalsController < ApplicationController
     end
   end
 
+  # GET /proposals/:uuid/votes
+  def votes
+    @proposal = Proposal.find_by_uuid(params[:uuid])
+    if @proposal.nil?
+      render json: { error: "Proposal not found" }, status: :not_found
+      return
+    else
+      @votes = @proposal.votes
+      render json: @votes
+    end
+  end
+
 end
