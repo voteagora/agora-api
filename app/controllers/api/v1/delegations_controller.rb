c0dd68aa136dbca6baf4ddb5f2336fa1ea4815a5
Kent Fenwick <kent.fenwick@gmail.com>
2023-06-07 12:38:25 -0400

    updated

--- app/controllers/api/v1/delegations_controller.rb
+++ app/controllers/api/v1/delegations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DelegationsController < ApiBaseController
 
   def delegates_by_token
-    @delegates = AddressStat.left_outer_joins(:balance).where("total_voting_power > 0").where("balances.token = :token", token: params[:token].upcase).order(total_voting_power: :desc)
+    @delegates = AddressStat.left_outer_joins(:balance).where("total_voting_power > 0").where("balances.token = :token", token: params[:token].upcase).order(total_voting_power: :desc).limit(100)
     render json: @delegates
   end
   
